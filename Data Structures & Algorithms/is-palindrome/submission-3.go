a1a8b3a66fda8c2a982fc52f3d98bcd91317870d
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2026-08-22 16:24:50 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.go
@@ -0,0 +1,36 @@
+func toLower(c byte) byte {
+    if c >= 'A' && c <= 'Z' {
+        return c + 32 // 'a' - 'A' = 32
+    }
+    return c
+}
+func isAlphanumeric(c byte) bool {
+    return (c >= 'a' && c <= 'z') || 
+           (c >= 'A' && c <= 'Z') || 
+           (c >= '0' && c <= '9')
+}
+
+func isPalindrome(s string) bool {
+	left, right := 0, len(s)-1
+    
+    for left < right {
+        
+        if!isAlphanumeric(s[left]) {
+            left++
+            continue
+        } else if!isAlphanumeric(s[right]) {
+            right--
+            continue
+        }
+        
+        
+        if toLower(s[left]) != toLower(s[right]) {
+            return false
+        }
+        
+        left++
+        right--
+    }
+
+	return true
+}
