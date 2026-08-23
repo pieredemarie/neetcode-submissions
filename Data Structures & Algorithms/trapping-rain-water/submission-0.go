0053f7ac0ae68904f85dddbaeb356720de643d2a
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2026-08-23 11:09:31 +0300

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.go
@@ -0,0 +1,26 @@
+func trap(height []int) int {
+	left, right := 0, len(height) - 1 
+	maxLeft, maxRight := 0,0 
+	res := 0
+
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] >= maxLeft {
+				maxLeft = height[left]
+			} else {
+				res += maxLeft - height[left]
+			}
+			left++
+		} else {
+			if height[right] >= maxRight {
+				maxRight = height[right]
+			}  else {
+				res += maxRight - height[right]
+			}
+
+			right--
+		}
+	}
+
+	return res
+}
