701f1b79d28e408827f726b8a9c4ab18d43047e3
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2026-08-26 13:29:01 +0300

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.go
@@ -0,0 +1,36 @@
+func countHours(piles []int, k int) int {
+	count := 0
+	for _, banana := range piles {
+		if banana > k {
+			count += banana/k
+			if banana % k != 0 {
+				count++
+			}
+		} else {
+			count++
+		}
+	}
+
+	return count 
+}
+func minEatingSpeed(piles []int, h int) int {
+	maxK := 0
+	for _, banana := range piles {
+		maxK = max(maxK, banana)
+	}
+
+	l, r := 1, maxK
+
+	for l < r {
+		mid := l + (r-l)/2
+		hours := countHours(piles,mid)
+
+		if hours <= h {
+			r = mid 
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return r
+}
