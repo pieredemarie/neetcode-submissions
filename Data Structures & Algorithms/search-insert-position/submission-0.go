fef4daa4d5eadef50902597c2b363d7cbafb79bc
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2026-09-03 08:37:00 +0300

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.go
@@ -0,0 +1,17 @@
+func searchInsert(nums []int, target int) int {
+	l, r := 0, len(nums) - 1
+
+	for l <= r {
+		m := (r-l)/2 + l
+
+		if nums[m] == target {
+			return m
+		} else if nums[m] > target {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+
+	return l
+}
